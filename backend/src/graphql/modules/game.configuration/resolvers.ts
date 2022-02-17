731f1f9bd761893ece2b0c55a431d19cb6e00394
Tim Stasse <tim.stasse@gmail.com>
2022-02-17 11:43:20 +1100

    Initial apollo server changes

---
+++ backend/src/graphql/modules/game.configuration/resolvers.ts
@@ -0,0 +1,39 @@
+import { nanoid } from 'nanoid';
+import { PubSub } from 'graphql-subscriptions';
+import { GameConfiguration, Resolvers } from './generated.types';
+import { Maybe } from '../generated.types';
+import { CLUSTER_NAME } from '../../../config';
+
+const pubsub = new PubSub();
+
+let gameConfig: Maybe<GameConfiguration> = null;
+
+const resolvers: Resolvers = {
+  Query: {
+    gameConfig: async () => gameConfig,
+  },
+  Mutation: {
+    connect: async () => {
+      if (gameConfig == null) {
+        gameConfig = {
+          uuid: nanoid(),
+          date: new Date().toJSON(),
+          cluster: CLUSTER_NAME,
+          state: 'LOBBY',
+        } as const;
+
+        pubsub.publish('GAME_STATE_UPDATED', gameConfig);
+      }
+
+      return gameConfig;
+    },
+  },
+  Subscription: {
+    gameConfig: {
+      subscribe: async () =>
+        pubsub.asyncIterableIterator(['GAME_STATE_UPDATED']),
+    },
+  },
+};
+
+export default resolvers;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,14 @@ require('make-promises-safe');
 
 import startServer from './server';
 import log from './log';
-import * as game from './stores/game';
-import { heartbeat } from './sockets';
 import config, { NODE_ENV } from './config';
+import { schema } from './graphql';
 
 async function main() {
   log.info(`bootstrapping game server in "${NODE_ENV}" mode`);
   log.trace('server config: %j', config);
 
-  await game.init();
-  await startServer();
-
-  heartbeat();
+  await startServer(schema);
 }
 
 main();
