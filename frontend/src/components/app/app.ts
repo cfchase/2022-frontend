bd7336011f12f7505ec650a44a021f1ca9b650e7
Benny Powers <web@bennypowers.com>
2022-02-21 16:07:45 -0500

    fix(frontend): connect via graphql-ws

--- frontend/src/components/app/app.ts
+++ frontend/src/components/app/app.ts
@@ -1,25 +1,31 @@
 import { LitElement, html, TemplateResult } from 'lit';
-import { ApolloQueryController, ApolloMutationController } from '@apollo-elements/core';
 import { customElement } from 'lit/decorators.js';
 
+import {
+  ApolloQueryController,
+  ApolloMutationController,
+  ApolloSubscriptionController,
+} from '@apollo-elements/core';
+
 import { GameConfigQuery } from './App.query.graphql.js';
+import { GameConfigUpdated } from './GameConfig.subscription.graphql.js';
 import { ConnectionRequestMutation } from './App.mutation.graphql.js';
 
 import style from './app.css';
 import shared from '../shared.css';
 
 export const getLocalStorage = () => {
-	return {
-    username: localStorage.getItem("username"),
-    gameId: localStorage.getItem("gameId"),
-    playerId: localStorage.getItem("playerId"),
-	};
+  return {
+    username: localStorage.getItem('username'),
+    gameId: localStorage.getItem('gameId'),
+    playerId: localStorage.getItem('playerId'),
+  };
 };
 
 export const updateLocalStorage = (gameId: string, playerId: string, username: string) => {
-	localStorage.setItem("username", username);
-	localStorage.setItem("gameId", gameId);
-	localStorage.setItem("playerId", playerId);
+  localStorage.setItem('username', username);
+  localStorage.setItem('gameId', gameId);
+  localStorage.setItem('playerId', playerId);
 };
 
 @customElement('apollo-app')
@@ -30,27 +36,30 @@ export class ApolloApp extends LitElement {
 
   gameConfigQuery = new ApolloQueryController(this, GameConfigQuery);
   connectionRequestMutation = new ApolloMutationController(this, ConnectionRequestMutation);
-
-  firstUpdated() {
-    this.sendConnectionRequest();
-  }
+  gameConfigSubscription = new ApolloSubscriptionController(this, GameConfigUpdated);
 
   public async sendConnectionRequest() {
     let storedGameConfig = {};
-		// get a previously connected player
-		const previousPlayer = getLocalStorage();
-		if (previousPlayer.gameId && previousPlayer.playerId && previousPlayer.username) {
-			storedGameConfig = previousPlayer;
-		}
-    const { data, error } = await this.connectionRequestMutation.mutate({ variables: storedGameConfig })
-    // store new player and game info
-    if (!error) updateLocalStorage(data);
-    // update the gameconfigQuery
-    this.gameConfigQuery.refetch();
+    // get a previously connected player
+    const previousPlayer = getLocalStorage();
+    if (previousPlayer.gameId && previousPlayer.playerId && previousPlayer.username)
+      storedGameConfig = previousPlayer;
+
+    try {
+      const { data } =
+        await this.connectionRequestMutation.mutate({ variables: storedGameConfig });
+      // store new player and game info
+      updateLocalStorage(data.connect.uuid, 'unknown playerID', 'unknown username');
+      // update the gameconfigQuery
+      this.gameConfigQuery.refetch();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render(): TemplateResult {
     return html`
+      <button @click="${this.sendConnectionRequest}">Connect</button>
       <dl>
         <dt>Pathname</dt>
         <dd>${this.gameConfigQuery.data?.location?.pathname ?? '/'}</dd>
@@ -60,6 +69,9 @@ export class ApolloApp extends LitElement {
         <dd>${this.gameConfigQuery.data?.gameConfig?.state}</dd>
         <dd>${this.gameConfigQuery.data?.gameConfig?.date}</dd>
       </dl>
+
+      <h2>Last Config Update</h2>
+      <output>${this.gameConfigSubscription.data?.gameConfig?.uuid}</output>
     `;
   }
 }
