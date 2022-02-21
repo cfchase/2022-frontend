01d81881859e988e9e758110694f62846c8ccf8b
Benny Powers <web@bennypowers.com>
2022-02-21 15:58:47 -0500

    fix: use graphql-ws for subscriptions
    this package is better maintained than subscriptions-transport-ws and
    has an esm version however, it will not work with graphql-playground until
    https://github.com/graphql/graphql-playground/pull/1295 lands

--- backend/src/plugins/health.ts
+++ backend/src/plugins/health.ts
@@ -1,34 +1,31 @@
-import { FastifyPluginCallback } from 'fastify';
-import fp from 'fastify-plugin';
+import type { WebSocketServer } from 'ws';
+import type { Request, Response } from 'express';
+
 import { uptime } from 'process';
 import humanize from 'humanize-duration';
 import log from '../log';
+
+// import { createRequire } from 'module'
+// const require = createRequire(import.meta.url);
 
 export interface HealthPluginOptions {
   version: string;
 }
 
-const healthPlugin: FastifyPluginCallback<HealthPluginOptions> = (
-  server,
-  options,
-  done
-) => {
-  log.info('mounting health plugin');
-  server.route({
-    method: 'GET',
-    url: '/health',
-    handler: async () => {
-      return {
-        connectedClients: server.websocketServer.clients.size,
-        status: 'ok',
-        uptime: humanize(uptime() * 1000),
-        serverTs: new Date().toJSON(),
-        version: options.version,
-      };
-    },
-  });
-
-  done();
-};
-
-export default fp(healthPlugin);
+export function healthCheck(wsServer: WebSocketServer) {
+  log.info('installing health middleware');
+  return async (_req: Request, res: Response) => {
+    // `wsServer` is a reference to the WebSocketServer instance
+    const connectedClients = wsServer.clients.size;
+    // if ts-node doesn't speak `require`, we'll bump `module` and use `createRequire`
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { version } = require('../../package.json');
+    res.json({
+      connectedClients,
+      status: 'ok',
+      uptime: humanize(uptime() * 1000),
+      serverTs: new Date().toJSON(),
+      version,
+    })
+  };
+}
