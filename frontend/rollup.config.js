e6ccb0cebdfff6f1a7ec6f323c26169587ef0314
Benny Powers <web@bennypowers.com>
2022-03-09 09:54:00 -0500

    fix(frontend): use latest rollup-plugin-html

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import html from '@open-wc/rollup-plugin-html';
+import html from '@web/rollup-plugin-html';
 import graphql from '@apollo-elements/rollup-plugin-graphql';
 import litcss from 'rollup-plugin-lit-css';
 import esbuild from 'rollup-plugin-esbuild';
