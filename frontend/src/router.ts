2212ebe629217ae3cf24bc62d36461fae6facf80
Benny Powers <web@bennypowers.com>
2022-03-15 00:12:35 -0400

    fix(frontend): import URLPattern polyfill by name

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,18 +1,19 @@
-import "urlpattern-polyfill";
+import { URLPattern } from 'urlpattern-polyfill';
 
-import { installRouter } from "pwa-helpers/router";
+import { installRouter } from 'pwa-helpers/router';
 
-import { makeVar } from "@apollo/client/core";
+import { makeVar } from '@apollo/client/core';
 
-const adminPattern = new URLPattern({ pathname: "/admin*" });
-const adminGamesPattern = new URLPattern({ pathname: "/admin/games/:gameId" });
+const adminPattern = new URLPattern({ pathname: '/admin*' });
+const adminGamesPattern = new URLPattern({ pathname: '/admin/games/:gameId' });
 
 const getLocation = (loc = window.location) => ({
   ...window.location,
   admin: adminPattern.test(window.location),
-  games: adminGamesPattern.exec(window.location)?.pathname?.groups
+  games: adminGamesPattern.exec(window.location)?.pathname?.groups,
 });
 
 export const locationVar = makeVar(getLocation());
 
 installRouter(loc => locationVar(getLocation(loc)));
+
